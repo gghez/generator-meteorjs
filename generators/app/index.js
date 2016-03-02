7791aff5c46d39aa18eb8937485fae7d745c8c61
gghez <gregory.ghez@gmail.com>
2016-03-02 01:02:22 +0100

    First draft.

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,7 +3,8 @@ var generators = require('yeoman-generator'),
     Promise = require('bluebird'),
     fsAsync = Promise.promisifyAll(fs),
     path = require('path'),
-    chalk = require('chalk');
+    chalk = require('chalk'),
+    _ = require('underscore');
 
 module.exports = generators.Base.extend({
     
@@ -18,6 +19,9 @@ module.exports = generators.Base.extend({
 
 	this.scriptSuffix = this.options.coffee ? '.coffee' : '.js';
 	this.appName = this.name || process.cwd().split(path.sep).pop();
+
+	this.should = {};
+	this.questions = [];
     },
     
     initializing: function(){
@@ -29,6 +33,67 @@ module.exports = generators.Base.extend({
 	    this.env.error(chalk.bold.red('Cannot configure a Meteor application inside another.'));
 	}
     },
+
+    prompting: {
+
+	noob: function(){
+	    this.questions.push({
+		type: 'checkbox',
+		name: 'remove_defaults',
+		message: 'Remove default packages',
+		choices:[
+		    {name: 'insecure', checked:true},
+		    {name: 'autopublish', checked: true}
+		]
+	    });
+	},
+
+	accounts: function(){
+	    this.questions.push({
+		type: 'checkbox',
+		name: 'accounts',
+		message: 'Accounts related packages',
+		choices:[
+		    {name: 'accounts-password', checked: true},
+		    {name: 'accounts-facebook'},
+		    {name: 'accounts-twitter'},
+		    {name: 'accounts-google'},
+		    {name: 'accounts-ui', checked: true}
+		]
+	    });
+	},
+
+	styles: function(){
+	    this.questions.push({
+		type: 'list',
+		name: 'styles',
+		message: 'HTML Styles generator',
+		choices:['less', 'fourseven:scss', '<none>'],
+		default: 'less'
+	    });
+	},
+
+	router: function(){
+	    this.questions.push({
+		type    : 'confirm',
+		name    : 'flowrouter',
+		message : 'Add FlowRouter as application router (kadira:flowrouter)',
+		default : true
+	    });
+	},
+	
+	ask: function(){
+	    var done = this.async();
+	    this.prompt(this.questions, (answers) => {
+		if (this.options.verbose){
+		    this.log(chalk.cyan('Answers:'), JSON.stringify(answers));
+		}
+		
+		_.assign(this.should, answers);
+		done();
+	    });
+	}
+    },
     
     configuring: {
 	
@@ -37,7 +102,7 @@ module.exports = generators.Base.extend({
 	    this.spawnCommandSync('meteor', ['create', this.name || '.']);
 	},
 	
-	package: function(){
+	packages: function(){
 	    if (!this.options.packages) return;
 
 	    if (this.name) {
@@ -55,6 +120,45 @@ module.exports = generators.Base.extend({
 		}
 	    });
 	    
+	},
+
+	scripting: function(){
+	    if (this.options.coffee){
+		this.log(chalk.bold.yellow('CoffeeScript support'));
+		this.spawnCommandSync('meteor', ['add', 'coffeescript']);
+	    }
+	},
+
+	remove_defaults: function(){
+	    if (this.should.remove_defaults.length){
+		this.log(chalk.bold.yellow('Remove defaults'));
+		this.spawnCommandSync('meteor', ['remove'].concat(this.should.remove_defaults));
+	    }
+	},
+
+	styles: function(){
+	    switch (this.should.styles){
+	    case '<none>':
+		break;
+	    default:
+		this.log(chalk.bold.yellow('Application styles generator:'), chalk.bold.white(this.should.styles));
+		this.spawnCommandSync('meteor', ['add', this.should.styles]);
+		break;
+	    }
+	},
+
+	accounts: function(){
+	    if (this.should.accounts.length){
+		this.log(chalk.bold.yellow('Accounts configuration'));
+		this.spawnCommandSync('meteor', ['add'].concat(this.should.accounts));
+	    }	    
+	},
+
+	router: function(){
+	    if (this.should.flowrouter){
+		this.log(chalk.bold.yellow('Router configuration'));
+		this.spawnCommandSync('meteor', ['add', 'kadira:flow-router', 'kadira:blaze-layout']);
+	    }
 	}
     },
     

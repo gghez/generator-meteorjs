6af928a306f2b6e499b24c11cd549ecc0f6fe9c2
gghez <gregory.ghez@thomsonreuters.com>
2016-03-03 11:44:54 +0100

    first draft

---
+++ generators/generator-base.js
@@ -0,0 +1,27 @@
+var generators = require('yeoman-generator'),
+    chalk = require('chalk');
+
+var MeteorJSGenerator = generators.Base.extend({
+    fail: function(msg){
+	this.env.error(chalk.bold.red(msg));
+    },
+    step1: function(name, text){
+	logs = [chalk.bold.yellow(name)];
+	if (text) logs.push(chalk.bold.white(text));
+
+	this.log.apply(this.log, logs);
+    },
+    step2: function(name, text){
+	logs = [chalk.yellow(name)];
+	if (text) logs.push(chalk.bold.white(text));
+
+	this.log.apply(this.log, logs);
+    },
+    debug: function(){
+	if (this.options.debug){
+	    this.log.apply(this.log, Array.prototype.map.call(arguments, chalk.cyan));
+	}
+    }
+});
+
+module.exports = MeteorJSGenerator;
